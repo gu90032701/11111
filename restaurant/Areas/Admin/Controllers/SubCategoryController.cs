8ffb8e955a3a456872e9ed35b50e615b9ab58efd
gu90032701 <gu900327@hotmail.com>
2020-03-25 15:04:25 -0500

    categoryandsubcategoryviewmodel

---
+++ restaurant/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restaurant.Data;
+
+namespace restaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // get-index
+        public async Task<IActionResult> Index()
+        {
+            var subCategories = await _db.SubCategory.Include(s=>s.Category).ToListAsync();
+            return View(subCategories);
+        }
+    }
+}
--- restaurant/Data/Migrations/20200321164811_subcategorymodel.Designer.cs
+++ restaurant/Data/Migrations/20200321164811_subcategorymodel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using restaurant.Data;
 
 namespace restaurant.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200321164811_subcategorymodel")]
+    partial class subcategorymodel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
