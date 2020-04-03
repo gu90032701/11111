bf8590fff46618d4526df68f80dd9f747671df05
gu90032701 <gu900327@hotmail.com>
2020-04-02 20:14:44 -0500

    subcategory get

--- restaurant/Areas/Admin/Controllers/SubCategoryController.cs
+++ restaurant/Areas/Admin/Controllers/SubCategoryController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using restaurant.Data;
+using restaurant.Models;
 using restaurant.Models.ViewModels;
 
 namespace restaurant.Areas.Admin.Controllers
@@ -22,7 +24,7 @@ namespace restaurant.Areas.Admin.Controllers
         // get-index
         public async Task<IActionResult> Index()
         {
-            var subCategories = await _db.SubCategory.Include(s=>s.Category).ToListAsync();
+            var subCategories = await _db.SubCategory.Include(s => s.Category).ToListAsync();
             return View(subCategories);
         }
         //get-create
@@ -40,12 +42,12 @@ namespace restaurant.Areas.Admin.Controllers
         //post-create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult>Create(CategoryAndSubCategoryViewModel model)
+        public async Task<IActionResult> Create(CategoryAndSubCategoryViewModel model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var doesSubcategoryExists = _db.SubCategory.Include(s => s.Category).Where(s => s.Name == model.SubCategory.Name && s.Category.Id == model.SubCategory.CategoryId);
-                    if(doesSubcategoryExists.Count()>0)
+                if (doesSubcategoryExists.Count() > 0)
                 {
                     StatusMessage = "Error：Sub category exists under" + doesSubcategoryExists.First().Category.Name + " category. please use another name";
                 }
@@ -61,9 +63,18 @@ namespace restaurant.Areas.Admin.Controllers
                 CategoryList = await _db.Category.ToListAsync(),
                 SubCategory = model.SubCategory,
                 SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).ToListAsync(),
-                StatusMessage=StatusMessage
+                StatusMessage = StatusMessage
             };
             return View(modelVM);
+        }
+        [ActionName("GetSubCategory")]
+        public async Task<IActionResult> GetSubCategory(int id)
+        {
+            List<SubCategory> subCategories = new List<SubCategory>();
+            subCategories = await (from subCategory in _db.SubCategory
+                                   where subCategory.CategoryId == id
+                                   select subCategory).ToListAsync();
+            return Json(new SelectList(subCategories, "Id", "Name"));
         }
     }
 }
